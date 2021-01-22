0cb3ace872ae82827ce21b1c828d3c32b6a0f72a
Hasan-star-creat <74924051+Hasan-star-creat@users.noreply.github.com>
2021-01-22 11:30:03 +0600

    coment out step by step

--- assignment.js
+++ assignment.js
@@ -2,82 +2,87 @@
   
 /************* first problem kilometer to meter *************/
      function kilometerToMeter(kilo){
-      if(kilo < 0){
+      if(kilo < 0){ // this condition negative value return 0
           return 0;
       }
-      let meter = kilo * 1000;
+      let meter = kilo * 1000; 
       return meter;
   }
-  let kilo = 1.5;
-  let meterResult = kilometerToMeter(kilo);
-  console.log(meterResult);
+    let kilo = 1.5; // input parameter
+    let meterResult = kilometerToMeter(kilo); //kilometerToMeter  function calling 
+  console.log(meterResult); // output kilomter to meter
 
   /************ second problem budgetCalcultor  **************/ 
   function budgetCalculator(watch,mobile,leptop){
-      if(watchQuantity <0){
+      if(watchQuantity <0){ // this condition negative pargameter return 0
           watchQuantity = 0;
       }
-      if (mobileQuantity <0){
+      let watchPrice = watchQuantity * 50;
+
+      if (mobileQuantity <0){ // this condition negative pargameter return 0
           mobileQuantity = 0;
     }
-    if (leptopQuantity < 0){
+    let mobilePrice = mobileQuantity * 100;
+
+    if (leptopQuantity < 0){  // this condition negative pargameter return 0
         leptopQuantity = 0;
     }
-     let watchPrice = watchQuantity * 50;
-     let mobilePrice = mobileQuantity * 100;
      let leptopPrice = leptopQuantity * 500;
-     let totalAmount = watchPrice + mobilePrice + leptopPrice;
-     return totalAmount;  
-  } 
-    let watchQuantity = -2;
-    let mobileQuantity = 3;
-    let leptopQuantity = 1; 
-    let result = budgetCalculator(watchQuantity, mobileQuantity,leptopQuantity);
-    console.log(result);
 
-    /***************** Problem Number Three  HotleCost Managment **************/
+     let totalAmount = watchPrice + mobilePrice + leptopPrice; // add total cost
+     return totalAmount;  // tunction output return 
+  } 
+    let watchQuantity = 2; //watch quentity input parameter 
+    let mobileQuantity = 4; // mobile quentity input parameter
+    let leptopQuantity = 1; // leptop quentity input dparameter
+    let budgetCalresult = budgetCalculator(watchQuantity, mobileQuantity,leptopQuantity); //function calling 
+    console.log(budgetCalresult);
+
+    /***************** Problem Number Of Three  HotleCost Managment **************/
      function hotelCost(days){
          let totalRent = 0;
-         while(days <0){ // this code need to negative days
-             return 0;
+         while(days <0){ // this condition negative pargameter return 0
          }
-         if(days <= 10){                    // ofer no one this condition 
+            // first offer this condition  
+         if(days <= 10){                 
             let totalRent = days * 100; 
              return totalRent;
          }
-         if(days <= 20){                           // ofer no two  this condition 
-             let firstOfer = 10 * 100;
-             let remaining = days - 10;
-             let secondOfer = remaining * 80;
-             let totalRent = firstOfer + secondOfer;
+          // second offer  this condition 
+         if(days <= 20){    
+             let firstoffer = 10 * 100;
+             let remaining = days - 10; // find conditioning days
+             let secondoffer = remaining * 80;
+             let totalRent = firstoffer + secondoffer;// second condition total rent
              return totalRent;
          }
+           //third  offer else condition 
          else{
-             let firstOfer = 10 * 100;  // ofer no three  defoult condition 
-             let secondOfer = 10 * 80;
-             let remaining  = days - 20;
-             let thirdOfer = remaining * 50;
-             let totalRent = firstOfer + secondOfer + thirdOfer;
-             return totalRent;
+             let firstoffer = 10 * 100;  
+             let secondoffer = 10 * 80;
+             let remaining  = days - 20; //find conditioning days
+             let thirdoffer = remaining * 50;
+             let totalRent = firstoffer + secondoffer + thirdoffer; // third condition total rent
+             return totalRent; 
          }
      }
-            let days = 22; // input days 
-            let hotelResult = hotelCost(days); // function call 
+            let days = 22; // input days parameter
+            let hotelResult = hotelCost(days); // function calling 
             console.log(hotelResult); // hotel cost output 
   
      //******** Problem Number Four********************** 
          function megaFriend(str){ // create a function 
             let compare = [0];   
-            let  word = str.split(' '); // string divided by word
+            let  word = str.split(' '); // string divided by word by word
                for(let i = 0; i < word.length; i++){
                let largest  =  word[i];
                if(largest.length > compare.length){ // compare largest string
-                    compare = largest; 
+                    compare = largest;  // largest string stor
                } 
            }
            return compare;
          }
-         const str ='Turaa Najma Sohag  Nafis'; // array of string
+         const str ='Tura Naju Hasan Sohag  Nafi'; // array of string
          let megaResult = megaFriend(str); // function callin 
          console.log(megaResult);  // output megafriends 
 
