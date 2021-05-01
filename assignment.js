09c8fcc1f798799337c90ec26222735097120f19
Hasan-star-creat <74924051+Hasan-star-creat@users.noreply.github.com>
2021-05-02 04:08:01 +0600

    file apgrade becouse assignment resubmit

--- assignment.js
+++ assignment.js
@@ -1,89 +1,103 @@
-// https://github.com/Hasan-star-creat/js-assignment-one
-  
-/************* first problem kilometer to meter *************/
-     function kilometerToMeter(kilo){
-      if(kilo < 0){ // this condition negative value return 0
-          return 0;
-      }
-      let meter = kilo * 1000; 
-      return meter;
-  }
-    let kilo = 1.5; // input parameter
-    let meterResult = kilometerToMeter(kilo); //kilometerToMeter  function calling 
-  console.log(meterResult); // output kilomter to meter
-
-  /************ second problem budgetCalcultor  **************/ 
-  function budgetCalculator(watch,mobile,leptop){
-      if(watchQuantity <0){ // this condition negative pargameter return 0
-          watchQuantity = 0;
-      }
-      let watchPrice = watchQuantity * 50;
-
-      if (mobileQuantity <0){ // this condition negative pargameter return 0
-          mobileQuantity = 0;
-    }
-    let mobilePrice = mobileQuantity * 100;
-
-    if (leptopQuantity < 0){  // this condition negative pargameter return 0
-        leptopQuantity = 0;
-    }
-     let leptopPrice = leptopQuantity * 500;
-
-     let totalAmount = watchPrice + mobilePrice + leptopPrice; // add total cost
-     return totalAmount;  // tunction output return 
-  } 
-    let watchQuantity = 2; //watch quentity input parameter 
-    let mobileQuantity = 4; // mobile quentity input parameter
-    let leptopQuantity = 1; // leptop quentity input dparameter
-    let budgetCalresult = budgetCalculator(watchQuantity, mobileQuantity,leptopQuantity); //function calling 
-    console.log(budgetCalresult);
-
-    /***************** Problem Number Of Three  HotleCost Managment **************/
-     function hotelCost(days){ // 
-         let totalRent = 0;
-         while(days <0){ // this condition negative pargameter return 0
-         }
-            // first offer this condition  
-         if(days <= 10){                 
-            let totalRent = days * 100; 
-             return totalRent;
-         }
-          // second offer  this condition 
-         if(days <= 20){    
-             let firstoffer = 10 * 100;
-             let remaining = days - 10; // find conditioning days
-             let secondoffer = remaining * 80;
-             let totalRent = firstoffer + secondoffer;// second condition total rent
-             return totalRent;
-         }
-           //third  offer else condition 
-         else{
-             let firstoffer = 10 * 100;  
-             let secondoffer = 10 * 80;
-             let remaining  = days - 20; //find conditioning days
-             let thirdoffer = remaining * 50;
-             let totalRent = firstoffer + secondoffer + thirdoffer; // third condition total rent
-             return totalRent; 
-         }
+/******  kilometerTometer Function start from Here: *****/
+function kilometerToMeter(kilo){
+       // If value of kilometer is positive then convert kilometer to meter 
+     if(kilo > 0){
+        return  kilo * 1000  
      }
-            let days = 22; // input days parameter
-            let hotelResult = hotelCost(days); // function calling 
-            console.log(hotelResult); // hotel cost output 
-  
-     //******** Problem Number Four********************** 
-         function megaFriend(str){ // create a function 
-            let compare = [0];   
-            let  word = str.split(' '); // string divided by word by word
-               for(let i = 0; i < word.length; i++){
-               let largest  =  word[i];
-               if(largest.length > compare.length){ // compare largest string
-                    compare = largest;  // largest string stor
-               } 
-           }
-           return compare;
-         }
-         const str ='Tura Naju Hasan Sohag  Nafi'; // array of string
-         let megaResult = megaFriend(str); // function callin 
-         console.log(megaResult);  // output megafriends 
+        // If value of kilometer is positive then
+     else{
+        return "warning!! Enter value is negative!"
+     }
+}
 
-         /************* Program Is End ***********/
+
+ /***** budgetCalculator function start from Here:*******/
+  function budgetCalculator(watchNumbers,mobileNumbers,leptopNumbers){
+       watchNumbers = watchNumbers * 50;
+       mobileNumbers = mobileNumbers * 100;
+       leptopNumbers = leptopNumbers * 500;
+        
+       const allProducts = [watchNumbers , mobileNumbers , leptopNumbers];
+
+       for (let i = 0; i < allProducts.length; i++) {
+           const element = allProducts[i];
+           var negetiveProduct = 0;
+           if(element < 1){
+             negetiveProduct++;
+             break;
+           } 
+       }
+                 // if any product number is negative following then return warnign message ;
+              if(negetiveProduct > 0){
+                  return "Warning!! your any product quantity  of negative!"
+              } 
+              // if product number doesn't negative following then return total budget;
+              else{
+                  let totalCalCultaor = watchNumbers + mobileNumbers + leptopNumbers ;
+                  return totalCalCultaor;
+         }
+  }
+
+
+         /****  hotelCost function start here    *****/
+         function hotelCost(days){
+           days =  Math.round(days);
+            //If the day with a negative number then the following warning will be returned:
+           if(days < 1){
+               return "Warning!! it is not possible have negative days";
+               // if the days doesn't negative days then calcultation total days costing ;
+           }else{
+                var totalCost = 0;
+            if(days <= 10){
+                 totalCost = days * 100;
+            }
+             else if(days <= 20){
+                var first_10_days = 10 * 100;
+                var after_10_days = (days - 10) * 80 ;
+                totalCost = first_10_days + after_10_days;
+            }else{
+                var first_10_days = 10 * 100;
+                var second_10_days = 10 * 80;
+                var after_20_days = (days - 20) * 50;
+                totalCost = first_10_days + second_10_days + after_20_days;
+            }
+           }
+     return totalCost;
+         }
+         
+
+         /**** megaFriend function start from here ****/
+           function megaFriend(name){
+            var perNameLetters = [];
+               var largeName = [];
+               for (let i = 0; i < name.length; i++) {
+                   var chack = name[i];
+                    var spaceCount = 0;
+                     
+                   for (let j = 0; j < chack.length; j++) {
+                       var chackSpace = chack[j].split("");
+
+                       if(chackSpace === " "){
+                           spaceCount = spaceCount + 1;
+                       }
+                   }
+
+                   var letters = name[i].length - spaceCount;
+                   if(letters > perNameLetters){
+                       perNameLetters = letters;
+                       largeName = name[i];
+                   }
+             
+               }
+                // This warning Show if there isn't element in this array 
+                 if(perNameLetters === 0){
+                     return "warning!! please insert your friends name in array! ";
+                 }
+                 // the big name will show the comparison:
+                 else{
+                    return largeName;
+                 } 
+           }
+
+      /************* Program Is End ***********/
+   
